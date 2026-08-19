f294d46591b5cb9ea6104f49fc19bfaf9316fdbe
Vitorgutierrezzxcv <comercialvittorgutierrez@gmail.com>
2026-08-19 14:25:39 -0300

    Adiciona marcador de versão do dual dispatch

---
+++ api/runtime-version.js
@@ -0,0 +1,9 @@
+export default async function handler(_req, res) {
+  res.setHeader('cache-control', 'no-store');
+  return res.status(200).json({
+    ok: true,
+    version: 'dual-dispatch-v1',
+    maxConcurrentCalls: 2,
+    secondCallEtaCapMinutes: 60,
+  });
+}
